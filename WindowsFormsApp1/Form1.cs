07198abc0796819acafeb7ef480868c926889f6d
Dima <garry20121@yandex.ru>
2019-04-11 13:00:22 +0300

    init

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (Opacity != 1) Opacity += 0.1;
+            else timer1.Enabled = false;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GraphicsPath gr = new GraphicsPath();//Создаем Регион
+            this.BackColor = Color.Aqua;//Цвет
+            gr.AddString("Жизнь слишком коротка,", Font.FontFamily, 2, 32, new Point(10, 10), StringFormat.GenericDefault);//Добавление текста
+            gr.AddString("начинайте с десерта", Font.FontFamily, 2, 32, new Point(10, 42), StringFormat.GenericDefault);
+            Rectangle rct = new Rectangle(130, 152, 250, 155);//Создание прямогоугльника
+            gr.AddRectangle(rct);//Рисование прямоугольника
+            gr.AddLine(130, 152, 194, 102);//Рисуем линию
+            gr.AddLine(194, 102, 380, 152);
+            gr.AddLine(380, 152, 130, 152);
+            Rectangle rct1 = new Rectangle(135, 152, 240, 3);//Создание прямогоугльника
+            gr.AddRectangle(rct1);//Рисование прямоугольника
+            Rectangle rct2 = new Rectangle(135, 217, 240, 35);//Создание прямогоугльника
+            gr.AddRectangle(rct2);//Рисование прямоугольника
+            gr.AddEllipse(25, 265, 490, 70);//Рисуем круг
+            gr.AddEllipse(20, 260, 500, 80); 
+            Region r = new Region(gr);
+            this.Region = r;//Отображение именно этого региона
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+ 
+        }
+    }
+}
---
+++ WindowsFormsApp1/Zastavka2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Zastavka2 : Form
+    {
+        public Zastavka2()
+        {
+            InitializeComponent();
+        }
+
+        private void Zastavka2_Load(object sender, EventArgs e)
+        {
+            AllowTransparency = true;// Позволяе сделать фон формы прозрачным
+                TransparencyKey = Color.White;// Указывает какой цвет
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (Opacity != 1) Opacity += 0.1;//Прозрачность
+            else
+            {
+                timer1.Enabled = false;//Выключам таймер
+                Form1 Fr = new Form1();
+                Fr.Show();//Переход на новую форму
+                Hide();//Скрытие старой
+            }
+
+        }
+    }
+}
